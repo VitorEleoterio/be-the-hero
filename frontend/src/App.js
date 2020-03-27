2a80c4aef5be5851fde62c0d4af342a2a08cd25e
Vitor E <vitor.rodrigues@al.resilia.com.br>
2020-03-26 23:10:25 -0300

    Criacao BackEnd

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+        <
+        p >
+        Eleonegga!!!
+        <
+        /p>
+
+        <
+        a className = "App-link"
+        href = "https://reactjs.org"
+        target = "_blank"
+        rel = "noopener noreferrer" >
+        OS MANOS <
+        /a> < /
+        header > <
+        /div>
+    );
+}
+
+export default App;
