39683411389c4c9c59ceec3f77b95e76a1326e4f
Vitor E <vitor.rodrigues@al.resilia.com.br>
2020-03-29 01:24:24 -0300

    The web page is almost finished and the connection of the frontend to the backend is in its final stage.

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi'
 
+import api from '../../services/api'
 
 import './styles.css';
 
