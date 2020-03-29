02b5a57134405fb8240cf1b65faee2a2d498b915
Vitor E <vitor.rodrigues@al.resilia.com.br>
2020-03-28 21:26:55 -0300

    The frontend pages were all created, the mission now is to create the backend connection

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi'
 
 import './styles.css';
 
+import logoImg from '../../assets/logo.svg';
+
 export default function NewIncident(){
-    return <h1>New Incident</h1>
+    return (
+        <div className="new-incident-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Be The Hero"/>
+
+                    <h1>Cadastrar novo caso</h1>
+                    <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
+
+                    <Link className="back-link" to="/profile">
+                    <FiArrowLeft size={16} color="#e02041"/>
+                    Voltar para home
+                </Link>
+                </section>
+
+                <form>
+                    <input placeholder="Titulo do caso"/>
+                    <textarea placeholder="Escreva seu caso"/>
+
+                    <input placeholder="Valor em reais"/>
+
+                    <button className="button" type="submit">Cadastrar</button>
+                </form>
+
+            </div>
+        </div>
+    );
 }
